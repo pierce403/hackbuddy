184ffb9ebc7eaa6d6b39b81140d93318660b6a49
Dean Pierce <pierce403@gmail.com>
2021-10-18 01:00:13 -0700

    make table good

---
+++ static/sesh.js
@@ -0,0 +1,42 @@
+
+
+function seshList()
+{      
+  console.log("Running new search");
+  
+  // clear table before starting search
+  let seshTable = document.getElementById("sesh-table");
+  seshTable.innerHTML = ""
+
+  seshList = fetch('list')
+    .then(response => response.json())
+    .then(function(data){
+       let seshTable = document.getElementById("sesh-table");
+       console.log(data);
+       seshTable.innerHTML = ""
+
+  for(let sesh of data)
+  {
+   console.log(sesh.description);
+   let row = seshTable.insertRow(-1);
+
+   row.insertCell().innerText = sesh.user;
+   row.insertCell().innerText = sesh.description;
+   row.insertCell().innerText = sesh.url;
+  }
+
+  })
+}
+
+seshList();
+
+//setInterval(function(){
+//  console.log("LOOPING!!");
+// }, 10000);//run this thang every 2 seconds
+
+
+
+
+
+
+
